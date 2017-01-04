4838667449d28b7b4f77ee559cbac4f904b1e51b
Gregg Reynolds <reynolds-gregg@norc.org>
2017-01-04 13:11:01 -0600

    project.clj

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :url "https://github.com/mobileink/polymer.gold"
   :license {:name "EPL v1.0 License"
             :url "http://opensource.org/licenses/eclipse-1.0.php"}
-  :dependencies [[org.clojure/clojure "1.8.0-RC4"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [miraj/markup "0.1.0-SNAPSHOT"]])
                  ;; [polymer.elements/gold "1.2.3-SNAPSHOT"]])
